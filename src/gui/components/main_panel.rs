c4cd1566f53684fd02b79c6abdcee6a747652f50
maefall <contact@maefall.com>
2024-12-24 16:57:30 +0100

    Demo conversion calculator

--- src/gui/components/main_panel.rs
+++ src/gui/components/main_panel.rs
@@ -1,16 +1,18 @@
-use super::Pixel360Measurement;
+use super::{Pixel360Measurement, SensConversion};
 use eframe::Frame;
 use egui::{CentralPanel, Context};
 
 #[derive(Default)]
 pub(crate) struct MainPanel {
     pixel_360_measurement: Pixel360Measurement,
+    sens_conversion: SensConversion,
 }
 
 impl eframe::App for MainPanel {
     fn update(&mut self, ctx: &Context, _frame: &mut Frame) {
         CentralPanel::default().show(ctx, |ui| {
             self.pixel_360_measurement.show(ui);
+            self.sens_conversion.show(ui);
         });
     }
 }
--- src/gui/components/mod.rs
+++ src/gui/components/mod.rs
@@ -1,5 +1,8 @@
 mod pixel_360_measurement;
 pub(crate) use pixel_360_measurement::Pixel360Measurement;
 
+mod sens_conversion;
+pub(crate) use sens_conversion::SensConversion;
+
 mod main_panel;
 pub(crate) use main_panel::MainPanel;
