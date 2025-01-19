6d941e1262c234e915e6278e61995a2aae8a3581
maefall <contact@maefall.com>
2025-01-19 19:08:57 +0100

    Scarlet frame

--- src/gui/widgets/sensitivity_conversion_demo.rs
+++ src/gui/widgets/sensitivity_conversion_demo.rs
@@ -1,4 +1,5 @@
 use egui::{TextEdit, Ui};
+use scarlet_frame::egui::Group;
 
 #[derive(Default)]
 pub(crate) struct SensitivityConversionDemo {
@@ -10,36 +11,38 @@ pub(crate) struct SensitivityConversionDemo {
 
 impl SensitivityConversionDemo {
     pub(crate) fn show(&mut self, ui: &mut Ui) {
-        ui.vertical(|ui| {
-            ui.heading("Sensitivity Conversion");
+        Group::new("sensitivity-conversion", "9slice-test.png").show(ui, |ui| {
+            ui.vertical(|ui| {
+                ui.heading("Sensitivity Conversion");
 
-            ui.label("in-game sensitivity");
-            ui.add(TextEdit::singleline(&mut self.original_in_game_sensitivity));
+                ui.label("in-game sensitivity");
+                ui.add(TextEdit::singleline(&mut self.original_in_game_sensitivity));
 
-            ui.label("original pixel 360");
-            ui.add(TextEdit::singleline(&mut self.original_pixel_360));
+                ui.label("original pixel 360");
+                ui.add(TextEdit::singleline(&mut self.original_pixel_360));
 
-            ui.label("target pixel 360");
-            ui.add(TextEdit::singleline(&mut self.target_pixel_360));
+                ui.label("target pixel 360");
+                ui.add(TextEdit::singleline(&mut self.target_pixel_360));
 
-            if ui.button("Convert").clicked() {
-                if let Ok(original_in_game_sensitivity) =
-                    self.original_in_game_sensitivity.parse::<f64>()
-                {
-                    if let Ok(original_pixel_360) = self.original_pixel_360.parse::<f64>() {
-                        if let Ok(target_pixel_360) = self.target_pixel_360.parse::<f64>() {
-                            let d360_difference = target_pixel_360 / original_pixel_360;
+                if ui.button("Convert").clicked() {
+                    if let Ok(original_in_game_sensitivity) =
+                        self.original_in_game_sensitivity.parse::<f64>()
+                    {
+                        if let Ok(original_pixel_360) = self.original_pixel_360.parse::<f64>() {
+                            if let Ok(target_pixel_360) = self.target_pixel_360.parse::<f64>() {
+                                let d360_difference = target_pixel_360 / original_pixel_360;
 
-                            self.converted_sensitivity =
-                                original_in_game_sensitivity * d360_difference;
+                                self.converted_sensitivity =
+                                    original_in_game_sensitivity * d360_difference;
+                            }
                         }
                     }
                 }
-            }
 
-            ui.label(format!("{}", self.converted_sensitivity));
+                ui.label(self.converted_sensitivity.to_string());
 
-            ui.separator();
+                ui.separator();
+            });
         });
     }
 }
--- src/gui/widgets/yaw_sweep.rs
+++ src/gui/widgets/yaw_sweep.rs
@@ -2,6 +2,7 @@ use super::{KeybindActionLabel, StatusLabel, WidgetState};
 use crate::start;
 use egui::{Align, Color32, Layout, Response, RichText, TextEdit, Ui, Widget};
 use lazy_static::lazy_static;
+use scarlet_frame::egui::Group;
 use std::sync::mpsc::{Receiver, Sender};
 use std::sync::{Arc, Mutex};
 
@@ -70,119 +71,123 @@ pub(crate) struct YawSweep {}
 
 impl Widget for YawSweep {
     fn ui(self, ui: &mut Ui) -> Response {
-        let mut state = YawSweepState::load_or_default(ui, "");
+        Group::new("yaw-sweep-group", "9slice-test.png").show(ui, |ui| {
+            let mut state = YawSweepState::load_or_default(ui, "");
 
-        let (
-            measurement_status_receiver,
-            sweep_distance_measurement_receiver,
-            sweep_execution_distance_sender,
-            sweep_execution_status_receiver,
-        ) = (
-            state.measurement_status_receiver.lock().unwrap(),
-            state.sweep_distance_measurement_receiver.lock().unwrap(),
-            state.sweep_execution_distance_sender.lock().unwrap(),
-            state.sweep_execution_status_receiver.lock().unwrap(),
-        );
-
-        if let Ok(measurement_status) = measurement_status_receiver.try_recv() {
-            state.measurement_status = measurement_status;
-        }
-
-        if let Ok(sweep_execution_status) = sweep_execution_status_receiver.try_recv() {
-            state.sweep_execution_status = sweep_execution_status;
-        }
-
-        if let Ok(sweep_distance_measurement) = sweep_distance_measurement_receiver.try_recv() {
-            state.measured_sweep_distance = sweep_distance_measurement;
-        }
-
-        ui.with_layout(Layout::top_down(Align::Center), |ui| {
-            ui.label(
-                RichText::new("Yaw Sweep")
-                    .size(GROUP_HEADER_SIZE)
-                    .color(*GROUP_HEADER_SIZE_COLOR),
+            let (
+                measurement_status_receiver,
+                sweep_distance_measurement_receiver,
+                sweep_execution_distance_sender,
+                sweep_execution_status_receiver,
+            ) = (
+                state.measurement_status_receiver.lock().unwrap(),
+                state.sweep_distance_measurement_receiver.lock().unwrap(),
+                state.sweep_execution_distance_sender.lock().unwrap(),
+                state.sweep_execution_status_receiver.lock().unwrap(),
             );
 
-            ui.columns(2, |cols| {
-                cols[0].with_layout(Layout::left_to_right(Align::Min), |ui| {
-                    ui.with_layout(Layout::top_down(Align::Center), |ui| {
-                        ui.label(
-                            RichText::new("Measurement")
-                                .size(PARTITION_HEADER_SIZE)
-                                .color(*PARTITION_HEADER_COLOR),
-                        );
+            if let Ok(measurement_status) = measurement_status_receiver.try_recv() {
+                state.measurement_status = measurement_status;
+            }
 
-                        ui.label(format!(
-                            "Measured distance: {} px",
-                            state.measured_sweep_distance
-                        ));
+            if let Ok(sweep_execution_status) = sweep_execution_status_receiver.try_recv() {
+                state.sweep_execution_status = sweep_execution_status;
+            }
 
-                        ui.columns(2, |cols| {
-                            cols[0].with_layout(Layout::right_to_left(Align::Min), |ui| {
-                                ui.add(
-                                    KeybindActionLabel::builder("ALT + M", "to measure").build(),
-                                );
-                            });
+            if let Ok(sweep_distance_measurement) = sweep_distance_measurement_receiver.try_recv() {
+                state.measured_sweep_distance = sweep_distance_measurement;
+            }
 
-                            cols[1].add(
-                                StatusLabel::builder(state.measurement_status)
-                                    .size(INFO_LABEL_SIZE)
-                                    .build(),
+            ui.with_layout(Layout::top_down(Align::Center), |ui| {
+                ui.label(
+                    RichText::new("Yaw Sweep")
+                        .size(GROUP_HEADER_SIZE)
+                        .color(*GROUP_HEADER_SIZE_COLOR),
+                );
+
+                ui.columns(2, |cols| {
+                    cols[0].with_layout(Layout::left_to_right(Align::Min), |ui| {
+                        ui.with_layout(Layout::top_down(Align::Center), |ui| {
+                            ui.label(
+                                RichText::new("Measurement")
+                                    .size(PARTITION_HEADER_SIZE)
+                                    .color(*PARTITION_HEADER_COLOR),
                             );
-                        });
-                    });
-                });
 
-                cols[1].with_layout(Layout::right_to_left(Align::Min), |ui| {
-                    ui.with_layout(Layout::top_down(Align::Center), |ui| {
-                        ui.label(
-                            RichText::new("Execution")
-                                .size(PARTITION_HEADER_SIZE)
-                                .color(*PARTITION_HEADER_COLOR),
-                        );
+                            ui.label(format!(
+                                "Measured distance: {} px",
+                                state.measured_sweep_distance
+                            ));
 
-                        ui.with_layout(Layout::left_to_right(Align::Min), |ui| {
-                            ui.with_layout(Layout::top_down(Align::Min), |ui| {
-                                ui.label(
-                                    RichText::new("Sweep distance (px):")
-                                        .size(PARTITION_INNER_LABEL_SIZE),
-                                );
+                            ui.columns(2, |cols| {
+                                cols[0].with_layout(Layout::right_to_left(Align::Min), |ui| {
+                                    ui.add(
+                                        KeybindActionLabel::builder("ALT + M", "to measure")
+                                            .build(),
+                                    );
+                                });
 
-                                ui.add(
-                                    KeybindActionLabel::builder("ALT + X", "to perform a sweep")
-                                        .build(),
-                                );
-                            });
-
-                            ui.with_layout(Layout::top_down(Align::Center), |ui| {
-                                let do_360_pixels_text_edit = ui.add(TextEdit::singleline(
-                                    &mut state.sweep_execution_distance_buffer,
-                                ));
-
-                                if do_360_pixels_text_edit.changed() {
-                                    if let Ok(do_360_pixels) =
-                                        state.sweep_execution_distance_buffer.parse::<u32>()
-                                    {
-                                        sweep_execution_distance_sender
-                                            .send(do_360_pixels)
-                                            .unwrap();
-                                    }
-                                }
-
-                                ui.add(
-                                    StatusLabel::builder(state.sweep_execution_status)
+                                cols[1].add(
+                                    StatusLabel::builder(state.measurement_status)
                                         .size(INFO_LABEL_SIZE)
                                         .build(),
                                 );
                             });
                         });
                     });
+
+                    cols[1].with_layout(Layout::right_to_left(Align::Min), |ui| {
+                        ui.with_layout(Layout::top_down(Align::Center), |ui| {
+                            ui.label(
+                                RichText::new("Execution")
+                                    .size(PARTITION_HEADER_SIZE)
+                                    .color(*PARTITION_HEADER_COLOR),
+                            );
+
+                            ui.with_layout(Layout::left_to_right(Align::Min), |ui| {
+                                ui.with_layout(Layout::top_down(Align::Min), |ui| {
+                                    ui.label(
+                                        RichText::new("Sweep distance (px):")
+                                            .size(PARTITION_INNER_LABEL_SIZE),
+                                    );
+
+                                    ui.add(
+                                        KeybindActionLabel::builder(
+                                            "ALT + X",
+                                            "to perform a sweep",
+                                        )
+                                        .build(),
+                                    );
+                                });
+
+                                ui.with_layout(Layout::top_down(Align::Center), |ui| {
+                                    let do_360_pixels_text_edit = ui.add(TextEdit::singleline(
+                                        &mut state.sweep_execution_distance_buffer,
+                                    ));
+
+                                    if do_360_pixels_text_edit.changed() {
+                                        if let Ok(do_360_pixels) =
+                                            state.sweep_execution_distance_buffer.parse::<u32>()
+                                        {
+                                            sweep_execution_distance_sender
+                                                .send(do_360_pixels)
+                                                .unwrap();
+                                        }
+                                    }
+
+                                    ui.add(
+                                        StatusLabel::builder(state.sweep_execution_status)
+                                            .size(INFO_LABEL_SIZE)
+                                            .build(),
+                                    );
+                                });
+                            });
+                        });
+                    });
                 });
             });
-        });
 
-        state.clone().save_state(ui, "");
-
-        ui.separator()
+            state.clone().save_state(ui, "");
+        })
     }
 }
