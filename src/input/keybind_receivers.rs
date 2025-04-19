a78784704b88af6579533e6e9f6aa23054fe773d
maefall <contact@maefall.com>
2025-04-19 20:49:47 +0200

    Order insensitive keybinds

--- src/input/keybind_receivers.rs
+++ src/input/keybind_receivers.rs
@@ -1,4 +1,5 @@
 use evdev::{Device, InputEventKind, Key};
+use std::collections::HashSet;
 use std::sync::mpsc;
 use std::thread;
 
@@ -9,23 +10,33 @@ pub(crate) fn start_keybind_receivers(
     let (do_360_sender, do_360_receiver) = mpsc::channel::<()>();
 
     thread::spawn(move || {
-        let mut is_alt_down = false;
+        let mut keys_down: HashSet<Key> = HashSet::new();
 
         while let Ok(events) = device.fetch_events() {
             for event in events {
-                if let InputEventKind::Key(Key::KEY_LEFTALT) = event.kind() {
-                    is_alt_down = event.value() == 1;
-                }
-
-                if let InputEventKind::Key(Key::KEY_LEFTBRACE) = event.kind() {
-                    if event.value() == 1 && is_alt_down {
-                        start_tracking_key_sender.send(()).unwrap();
+                if let InputEventKind::Key(key) = event.kind() {
+                    match event.value() {
+                        1 => {
+                            keys_down.insert(key);
+                        }
+                        0 => {
+                            keys_down.remove(&key);
+                        }
+                        _ => {}
                     }
-                }
 
-                if let InputEventKind::Key(Key::KEY_RIGHTBRACE) = event.kind() {
-                    if event.value() == 1 && is_alt_down {
-                        do_360_sender.send(()).unwrap();
+                    // Alt + [
+                    if keys_down.contains(&Key::KEY_LEFTALT)
+                        && keys_down.contains(&Key::KEY_LEFTBRACE)
+                    {
+                        let _ = start_tracking_key_sender.send(());
+                    }
+
+                    // Alt + ]
+                    if keys_down.contains(&Key::KEY_LEFTALT)
+                        && keys_down.contains(&Key::KEY_RIGHTBRACE)
+                    {
+                        let _ = do_360_sender.send(());
                     }
                 }
             }
