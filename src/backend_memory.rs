8a16d8a7f7667fd723cfac1b1b49386b1e288ce7
maefall <contact@maefall.com>
2025-04-21 19:08:10 +0200

    Ditch cancelling

--- src/backend_memory.rs
+++ src/backend_memory.rs
@@ -3,13 +3,16 @@ use mmap_sync::synchronizer::Synchronizer;
 use named_sem::NamedSemaphore;
 use once_cell::sync::Lazy;
 use rkyv::{Archive, Deserialize, Serialize};
-use std::ffi::OsString;
+use std::{
+    ffi::OsString,
+    sync::atomic::{AtomicBool, Ordering},
+};
 use tokio::time::Duration;
-use tokio_util::sync::CancellationToken;
 
 static SHARED_MEMORY_FILE_PATH: Lazy<OsString> =
     Lazy::new(|| OsString::from("/dev/shm/refract-sm"));
 const SEMAPHORE_NAME: &str = "/refract-sem";
+static LISTENER_STARTED: AtomicBool = AtomicBool::new(false);
 
 #[derive(Archive, Serialize, Deserialize, Debug, PartialEq)]
 #[archive_attr(derive(CheckBytes))]
@@ -26,52 +29,48 @@ pub enum RefractEvent {
 }
 
 pub async fn test() {
-    let listener_cancellation_token = CancellationToken::new();
-
-    start_listener(
-        |archived_refract_event| match archived_refract_event {
-            ArchivedRefractEvent::Combo(combo) => match combo {
-                ArchivedComboEvent::Measure => {
-                    println!("measure")
-                }
-                ArchivedComboEvent::Perform360 => {
-                    println!("perform 360")
-                }
-            },
-            ArchivedRefractEvent::RelativeMouseMovement(movement) => {
-                println!("mouse movement: {movement}")
+    start_listener(|archived_refract_event| match archived_refract_event {
+        ArchivedRefractEvent::Combo(combo) => match combo {
+            ArchivedComboEvent::Measure => {
+                println!("measure")
+            }
+            ArchivedComboEvent::Perform360 => {
+                println!("perform 360")
             }
         },
-        listener_cancellation_token.clone(),
-    );
+        ArchivedRefractEvent::RelativeMouseMovement(movement) => {
+            println!("mouse movement: {movement}")
+        }
+    });
 
     write(&RefractEvent::Combo(ComboEvent::Measure));
     write(&RefractEvent::Combo(ComboEvent::Perform360));
 
     use tokio::time::{sleep, Duration};
 
-    sleep(Duration::from_secs(10)).await;
-
-    listener_cancellation_token.cancel();
+    sleep(Duration::from_secs(3)).await;
 
     write(&RefractEvent::RelativeMouseMovement(-1));
     write(&RefractEvent::RelativeMouseMovement(1));
 }
 
-fn start_listener<F>(mut handler: F, cancellation_token: CancellationToken)
+fn start_listener<F>(mut handler: F)
 where
     F: FnMut(&ArchivedRefractEvent) + Send + 'static,
 {
+    if LISTENER_STARTED
+        .compare_exchange(false, true, Ordering::SeqCst, Ordering::SeqCst)
+        .is_err()
+    {
+        panic!("Listener is already running");
+    }
+
     tokio::task::spawn_blocking(move || {
         let mut synchronizer = Synchronizer::new(&SHARED_MEMORY_FILE_PATH);
         let mut semaphore =
             NamedSemaphore::create(SEMAPHORE_NAME, 0).expect("Failed to create semaphore");
 
         loop {
-            if cancellation_token.is_cancelled() {
-                break;
-            }
-
             semaphore.wait().expect("Failed to wait for semaphore");
 
             let data = unsafe { synchronizer.read::<RefractEvent>(false) }.expect("read failed");
